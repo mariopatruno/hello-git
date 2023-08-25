60bcdf133b0fb169b20ade0acd414f3d16a37e26
skiury <mariopatruno005@gmail.com>
2023-08-25 12:19:08 +0200

    creation of function generaOggettoSubString

--- es1.ts
+++ es1.ts
@@ -8,3 +8,22 @@ function generaSubString(str: string): string[] {
     }
     return sottoStringhe;  // Restituisce l'array contenente tutte le sottostringhe
 }
+
+function generaOggettoSubString(inputArray: string[]): Record<string, string[]> {
+    let oggettoFinale: Record<string, string[]> = {};  // Crea un oggetto vuoto per immagazzinare i risultati
+  
+    for (const inputStringa of inputArray) {  // Itera attraverso ogni stringa nell'array di input
+      try {
+        if (typeof inputStringa !== 'string') {  // Verifica se l'elemento dell'array è una stringa
+          throw new Error('Input non è una stringa');  // Da errore se l'input non è una stringa
+        }
+  
+        let sottoStringhe = generaSubString(inputStringa);  // Genera le sottostringhe per la stringa corrente
+        oggettoFinale[inputStringa] = sottoStringhe;  // Aggiungi l'array di sottostringhe all'oggetto risultante
+      } catch (error) {
+        console.error('Errore: ', error.message);  // Gestisce eventuali errori e stampa il messaggio di errore
+      }
+    }
+  
+    return oggettoFinale;  // Restituisce l'oggetto contenente le chiavi e le sottostringhe associate
+  }
